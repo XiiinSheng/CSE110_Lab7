3fdb23cebaeb9f58369dc8e5fad9b81ebd282259
Xin Sheng <xisheng@ucsd.edu>
2021-11-07 13:57:29 -0800

    Finished explore

--- assets/scripts/Router.js
+++ assets/scripts/Router.js
@@ -38,6 +38,7 @@ export class Router {
      * router instance using the 'this' keyword. Substitute 'home' for the variable
      * page
      */
+    this[page] = pageFunc;
   }
 
   /**
@@ -65,5 +66,20 @@ export class Router {
      *     and URL + hash to history
      *  4. Finally, call the stored function for the given page
      */
+
+    if(this[page] == undefined){
+      return;
+    }
+
+    const hash = (page == 'home') ? '' : ('#'+page);
+    
+    if(!statePopped && window.location.hash != hash){
+      // console.log("Adding new history");
+      // console.log(window.location);
+      history.pushState({page}, page, window.location.origin + hash);
+      // console.log(history);
+    }
+
+    this[page]();
   }
 }
